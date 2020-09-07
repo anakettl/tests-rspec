1643389d4323b6fb01ca6f7ae00c56904b94d7c7
anakettl <anakettl@gmail.com>
2020-09-07 11:55:47 -0300

    feat(pokemon-full_name): Add new test to return nil when name and id is empty

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe '#full_name' do
-  it 'show name and id' do
+  #We use the hash sign (#) to refer to instance methods and the period (.) for us refer to class methods, as the convention says Ruby documentation
+  it 'show name and id when is filled' do
     pokemon = Pokemon.new(name: 'Charizard', id: 6)
     expect(pokemon.full_name).to eq('Charizard - 6')
+  end
+  #it is good practice to define only one assertion per test
+  it 'is nil when do not have name and id' do
+    pokemon = Pokemon.new
+    expect(pokemon.full_name).to be_nil
   end
 end
